64e103c7eb55687323fc711e756341249aa9322d
Erik Hasse <erik.g.hasse@gmail.com>
2025-12-03 12:11:17 -0600

    add solution for day 3 part 2

--- src/day3.clj
+++ src/day3.clj
@@ -10,17 +10,22 @@
 (defn get-max [x]
   (str (apply max (map ^[char] Character/getNumericValue x))))
 
-(defn get-first [x]
-  (get-max (subs x 0 (dec (count x)))))
+(defn get-rest [x next-char]
+  (apply str (drop (inc (str/index-of x next-char)) x)))
 
-(defn get-second [x first] (get-max (last (str/split x (re-pattern first) 2))))
+(defn get-next [x n]
+  (if (= 1 n)
+    [(get-max x)]
+    (let
+      [next-char (get-max (subs x 0 (- (count x) n -1)))]
+      (concat [next-char] (get-next (get-rest x next-char) (dec n))))))
 
-(defn compute-joltage [x]
-  (let [first (get-first x)
-        second (get-second x first)]
-    (parse-long (str first second))))
+(defn get-joltage [n x]
+  (parse-long (apply str (get-next x n))))
 
-(defn part1 [input]
-  (reduce + (map compute-joltage input)))
+(defn solve [input n]
+  (reduce + (map (partial get-joltage n) input)))
 
-(part1 input)
+
+(println "Part 1: " (solve input 2))
+(println "Part 2: " (solve input 12))
