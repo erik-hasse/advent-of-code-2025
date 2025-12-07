6b6c36c13adbf196d977ad208fbca2f0b2ccc394
Erik Hasse <erik.g.hasse@gmail.com>
2025-12-07 16:46:06 -0600

    solve day 7 part 2

--- src/day7.clj
+++ src/day7.clj
@@ -11,17 +11,39 @@
 (defn get-indices [s t]
   (set (keep-indexed (fn [i ch] (when (= ch t) i)) s)))
 
-(defn step [{splits :splits beams :beams} next]
+
+(defn accum-pairs [pairs]
+  (reduce (fn [acc [k v]] (update acc v (fnil conj #{}) k))
+          {} pairs))
+
+(defn get-new-beam-map [split-beams]
+  (accum-pairs (mapcat (fn [x] [[x (inc x)] [x (dec x)]]) split-beams)))
+
+(defn get-new-timelines [old-timelines new-beams]
+  (apply hash-map (mapcat
+                    (fn [[k v]]
+                      [k (reduce + (map #(get old-timelines % 0) v))])
+                    (get-new-beam-map new-beams))))
+
+(defn step [{splits :splits timelines :timelines} next]
   (let [splitters (get-indices next \^)
-        split-locs (set/intersection splitters beams)
-        continuing-beams (set/difference beams split-locs)
-        new-beams (set (mapcat (fn [x] #{(inc x) (dec x)}) split-locs))
-        all-beams (set/union continuing-beams new-beams)]
-    {:splits (+ splits (count split-locs)) :beams all-beams}))
+        existing-beams (set (keys timelines))
+        split-locs (set/intersection splitters existing-beams)
+        continuing-beams (set/difference existing-beams split-locs)
+        existing-timelines (select-keys timelines continuing-beams)
+        new-timelines (get-new-timelines timelines split-locs)
+        all-timelines (merge-with + existing-timelines new-timelines)]
+    {:splits (+ splits (count split-locs)) :timelines all-timelines}))
+
+(defn run [input]
+  (reduce step {:splits 0 :timelines (zipmap (get-indices (first input) \S) (repeat 1))} (rest input)))
 
 (defn part1 [input]
-  ((reduce step {:splits 0 :beams (set (get-indices (first input) \S))} (rest input)) :splits))
+  ((run input) :splits))
 
 (println "Part 1:" (time (part1 input)))
 
+(defn part2 [input]
+  (reduce + (vals ((run input) :timelines))))
 
+(println "Part 2:" (time (part2 input)))
